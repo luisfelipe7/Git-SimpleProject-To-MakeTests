90df490113a53d1cce03aa522613bac08f4874a8
Felipe Castro <luisfelipecs97@gmail.com>
2021-09-25 21:44:07 -0600

    Release v2.0 - Implemented One Person in the Main

--- Main.cpp
+++ Main.cpp
@@ -6,14 +6,25 @@ Information: Created to perform simple tests with Git
 
 */
 
-#include <iostream>
+#include "Person.cpp"
 
 using namespace std;
 
 int main()
 {
     // Saying Hello and explaining the creation of this class
-    cout << "Hello World, I am making this as a test to practice Git" << endl;
+    cout << "Hello World, I am making this as a test to practice Git Branches" << endl;
+
+    // Creating one person and testing the methods
+    Person p1("Yenifer","402300414",25);
+    p1.personalInformation();
+    if(!(p1.isLegalAge())){
+        cout<<"This person is having the legal age"<<endl;
+    }else{
+        cout<<"This person is not having the legal age"<<endl;
+    }
+    cout<<endl;
+    system("pause");
 
     return 0;
 }
