7833a9a32d22bbef3f815e372198b6814902a0e9
Felipe Castro <luisfelipecs97@gmail.com>
2021-09-25 21:30:38 -0600

    Implemented new methods on Person.cpp

--- Person.cpp
+++ Person.cpp
@@ -13,34 +13,41 @@ using namespace std;
 class Person
 {
 
-// Attributes
+    // Attributes
 private:
-string name;
-string id;
-int age;
+    string name;
+    string id;
+    int age;
 
-// Constructors
+    // Constructors
 public:
-// Without Parameters
-Person(){
-name = "";
-id = "";
-age = 0;
-}
-// With Parameters
-Person(string Name, string Id, int Age){
-name = Name;
-id = Id;
-age = Age;
-}
+    // Without Parameters
+    Person()
+    {
+        name = "";
+        id = "";
+        age = 0;
+    }
+    // With Parameters
+    Person(string Name, string Id, int Age)
+    {
+        name = Name;
+        id = Id;
+        age = Age;
+    }
 
-// Obtain person information
-void personalInformation(){
-cout << "Hello, this is my personal information:"<<endl;
-cout << "- Name : "<<name<<endl;
-cout << "- Id : "<<id<<endl;
-cout << "- Age : "<<age<<endl;
-}
+    // Obtain person information
+    void personalInformation()
+    {
+        cout << "Hello, this is my personal information:" << endl;
+        cout << "- Name : " << name << endl;
+        cout << "- Id : " << id << endl;
+        cout << "- Age : " << age << endl;
+    }
 
-
+    // Method to define if a person is "legal"
+    bool isLegalAge()
+    {
+        return (age < 18);
+    }
 };
