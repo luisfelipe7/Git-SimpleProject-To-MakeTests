c4efa9c334fe2d9efc9e16e7bfe369001219cae0
Felipe Castro <luisfelipecs97@gmail.com>
2021-09-25 20:53:21 -0600

    Created and Added Person.cpp (Pending to Implement)

---
+++ Person.cpp
@@ -0,0 +1,17 @@
+/*
+
+Author: Felipe Castro
+Class: Main.cpp
+Information: Created to perform simple tests with Git
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+class Person
+{
+
+    // Pending to Implement
+};
