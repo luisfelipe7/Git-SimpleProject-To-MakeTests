53db3031f1c6aa004aa401c6519ff27fc5c11a2b
Felipe Castro <luisfelipecs97@gmail.com>
2021-09-25 20:24:06 -0600

    Initial Commit, Simple Class with a Message and .gitignore

---
+++ Main.cpp
@@ -0,0 +1,18 @@
+/*
+
+Author: Felipe Castro
+Class: Main.cpp
+Information: Created to perform simple tests with Git
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    cout << "Hello all, I am making this as a test to practice Git" << endl;
+
+    return 0;
+}
